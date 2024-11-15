89374d2a3cf919fd06bda4c2c8ccbd171325635e
nemolucky <126289314+nemolucky@users.noreply.github.com>
2024-11-15 22:58:22 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -7,7 +7,7 @@ $md5_password = md5(password);
 $query = pg_query($db, "SELECT * FROM `users` WHERE `login` = '{$mail} AND `password`='{$md5_password}'");
 if (pg_num_rows($query) != 0) {
   $_SESSION['user'] = ['nick' => $mail];
-  header("Location: /user.php");
+  // header("Location: /user.php");
 }
 else{
   echo("Ошибка")
